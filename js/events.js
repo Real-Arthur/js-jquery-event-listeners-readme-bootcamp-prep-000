7e73935924eeb3b16db00ba38ab59b3a1c84a11a
Arthur Carson <carson.bob.arthur@gmail.com>
2018-02-01 06:09:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,21 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    
+  })
+  
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
 });
